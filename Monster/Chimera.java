bb5a9e07db9c2e7a6e172c57bba62789ba00be24
Teleejr <81259920+Teleejr@users.noreply.github.com>
2021-04-12 22:31:39 -0400

    Add Monster package

---
+++ Monster/Chimera.java
@@ -0,0 +1,13 @@
+package Monster;
+
+public class Chimera extends Monster {
+
+    public Chimera(String name) {
+        super(name);
+    }
+
+    @Override
+    public String attack() {
+        return "ATTACK WITH FIRE, WATER, AND ROCKS!!!!!!";
+    }
+}
